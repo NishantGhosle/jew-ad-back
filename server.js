8eec5e55d329d5b0639b935ce707939858485399
Nishant Ghosle <nishantghosle7@gmail.com>
2025-01-08 16:09:32 +0530

    lo

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ connectDB();
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
-app.get("/api/admin-credentials", (req, res) => {
-  res.json({
-    username: process.env.ADMIN_USERNAME,
-    password: process.env.ADMIN_PASSWORD,
-  });
-});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
